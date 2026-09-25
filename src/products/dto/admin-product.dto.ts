ff683bb58b456623d6e337cb13ef84eb68232bb7
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2026-09-24 22:42:11 -0400

    BEST SELLINGS PRODUCTS DTO

---
+++ src/products/dto/admin-product.dto.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class TopSellerProductDTO{
+
+    @ApiProperty({name:"id",description:"Product's id"})
+    id:string;
+
+    @ApiProperty({name:"title", description:"Product's Name"})
+    title:string;
+
+    @ApiProperty({name:"sku", description:"Product's SKU"})
+    sku:string;
+
+    @ApiProperty({name:"price", description:"Product's Price"})
+    price:number;
+
+    @ApiProperty({name:"stok", description:"Product's Current Stock"})
+    stok:number;
+
+}
+
+export class GetTopSellersResponseDTO{
+
+    @ApiProperty({name:"message", example:"Top 10 best-selling products"})
+    message:string;
+
+    @ApiProperty({name:"date"})
+    date:Date;
+
+    @ApiProperty({name:"products",type:[TopSellerProductDTO]})
+    products:TopSellerProductDTO[]
+
+}
